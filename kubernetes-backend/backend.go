464936a0b25b080b663ef60b996dbd6cd90a2ffd
taemon1337 <taemon1337@gmail.com>
2018-02-07 17:58:13 -0500

    added kube backend

---
+++ kubernetes-backend/backend.go
@@ -0,0 +1,103 @@
+package kubernetes
+
+import (
+	"errors"
+	"log"
+  "net/http"
+  "time"
+
+	"github.com/fabiolb/fabio/config"
+	"github.com/fabiolb/fabio/registry"
+)
+
+// be is an implementation of a registry backend for kubernetes.
+type be struct {
+	c     *http.Client
+	cfg   *config.Kubernetes
+	dereg map[string](chan bool)
+}
+
+func NewBackend(cfg *config.Kubernetes) (registry.Backend, error) {
+  client := &http.Client{Transport: &http.Transport{cfg.Transport}}
+
+  req := client.NewRequest("GET", cfg.Scheme + cfg.Addr, nil)
+  req.Header.Add("Bearer", cfg.Token)
+
+  res, err := client.Do(req)
+  if err != nil {
+    return nil, err
+  }
+
+	// we're good
+	log.Printf("[INFO] kubernetes: Connecting to Kubernetes at %s%s", cfs.Scheme, cfg.Addr)
+	return &be{c: client, cfg: cfg}, nil
+}
+
+func (b *be) Register(services []string) error {
+	log.Printf("[INFO] Kubernetes default registration of Fabio is not supported.")
+	return nil
+}
+
+func (b *be) Deregister(service string) error {
+	dereg := b.dereg[service]
+	if dereg == nil {
+		log.Printf("[WARN]: Attempted to deregister unknown service %q", service)
+		return nil
+	}
+	dereg <- true // trigger deregistration
+	<-dereg       // wait for completion
+	delete(b.dereg, service)
+
+	return nil
+}
+
+func (b *be) DeregisterAll() error {
+	log.Printf("[DEBUG]: kubernetes: Deregistering all registered aliases.")
+	for name, dereg := range b.dereg {
+		if dereg == nil {
+			continue
+		}
+		log.Printf("[INFO] kubernetes: Deregistering %q", name)
+		dereg <- true // trigger deregistration
+		<-dereg       // wait for completion
+	}
+	return nil
+}
+
+func (b *be) ManualPaths() ([]string, error) {
+	log.Printf("[INFO] Kubernetes No implementation for ManualPaths.")
+	return [], err
+}
+
+func (b *be) ReadManual(path string) (value string, version uint64, err error) {
+	log.Printf("[INFO] Kubernetes No implementation for ReadPaths.")
+	return (b.c, path, 0)
+}
+
+func (b *be) WriteManual(path string, value string, version uint64) (ok bool, err error) {
+	log.Printf("[INFO] Kubernetes No implementation for WriteManual.")
+  return (b.c, path, 0)
+}
+
+func (b *be) WatchServices() chan string {
+	log.Printf("[INFO] kubernetes: Watching all Kubernetes services")
+
+	svc := make(chan string)
+	go watchServices(b.c, b.cfg)
+	return svc
+}
+
+func (b *be) WatchManual() chan string {
+	log.Printf("[INFO] kubernetes: No implementation for WatchManual")
+
+	kv := make(chan string)
+	return kv
+}
+
+func (b *be) WatchNoRouteHTML() chan string {
+	log.Printf("[INFO] kubernetes: No implementation for WatchNoRouteHTML")
+
+	html := make(chan string)
+	return html
+}
+
---
+++ kubernetes-backend/service.go
@@ -0,0 +1,159 @@
+package kubernetes
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"runtime"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// watchServices monitors the kubernetes health checks and creates a new configuration
+// on every change.
+func watchServices(client *http.Client, config chan string) {
+	var lastIndex uint64
+
+	for {
+		q := &api.QueryOptions{RequireConsistent: true, WaitIndex: lastIndex}
+		checks, meta, err := client.Health().State("any", q)
+		if err != nil {
+			log.Printf("[WARN] kubernetes: Error fetching health state. %v", err)
+			time.Sleep(time.Second)
+			continue
+		}
+
+		log.Printf("[DEBUG] kubernetes: Health changed to #%d", meta.LastIndex)
+		config <- servicesConfig(client, passingServices(checks, status), tagPrefix)
+		lastIndex = meta.LastIndex
+	}
+}
+
+// servicesConfig determines which service instances have passing health checks
+// and then finds the ones which have tags with the right prefix to build the config from.
+func servicesConfig(client *api.Client, checks []*api.HealthCheck, tagPrefix string) string {
+	// map service name to list of service passing for which the health check is ok
+	m := map[string]map[string]bool{}
+	for _, check := range checks {
+		// Make the node part of the id, because according to the Kubernetes docs
+		// the ServiceID is unique per agent but not cluster wide
+		// https://www.kubernetes.io/api/agent/service.html#id
+		name, id := check.ServiceName, fmt.Sprintf("%s.%s", check.Node, check.ServiceID)
+
+		if _, ok := m[name]; !ok {
+			m[name] = map[string]bool{}
+		}
+		m[name][id] = true
+	}
+
+	var config []string
+	for name, passing := range m {
+		cfg := serviceConfig(client, name, passing, tagPrefix)
+		config = append(config, cfg...)
+	}
+
+	// sort config in reverse order to sort most specific config to the top
+	sort.Sort(sort.Reverse(sort.StringSlice(config)))
+
+	return strings.Join(config, "\n")
+}
+
+// serviceConfig constructs the config for all good instances of a single service.
+func serviceConfig(client *api.Client, name string, passing map[string]bool, tagPrefix string) (config []string) {
+	if name == "" || len(passing) == 0 {
+		return nil
+	}
+
+	dc, err := datacenter(client)
+	if err != nil {
+		log.Printf("[WARN] kubernetes: Error getting datacenter. %s", err)
+		return nil
+	}
+
+	q := &api.QueryOptions{RequireConsistent: true}
+	svcs, _, err := client.Catalog().Service(name, "", q)
+	if err != nil {
+		log.Printf("[WARN] kubernetes: Error getting catalog service %s. %v", name, err)
+		return nil
+	}
+
+	env := map[string]string{
+		"DC": dc,
+	}
+
+	for _, svc := range svcs {
+		// check if the instance is in the list of instances
+		// which passed the health check
+		if _, ok := passing[fmt.Sprintf("%s.%s", svc.Node, svc.ServiceID)]; !ok {
+			continue
+		}
+
+		// get all tags which do not have the tag prefix
+		var svctags []string
+		for _, tag := range svc.ServiceTags {
+			if !strings.HasPrefix(tag, tagPrefix) {
+				svctags = append(svctags, tag)
+			}
+		}
+
+		// generate route commands
+		for _, tag := range svc.ServiceTags {
+			if route, opts, ok := parseURLPrefixTag(tag, tagPrefix, env); ok {
+				name, addr, port := svc.ServiceName, svc.ServiceAddress, svc.ServicePort
+
+				// use kubernetes node address if service address is not set
+				if addr == "" {
+					addr = svc.Address
+				}
+
+				// add .local suffix on OSX for simple host names w/o domain
+				if runtime.GOOS == "darwin" && !strings.Contains(addr, ".") && !strings.HasSuffix(addr, ".local") {
+					addr += ".local"
+				}
+
+				// build route command
+				weight := ""
+				ropts := []string{}
+				tags := strings.Join(svctags, ",")
+				addr = net.JoinHostPort(addr, strconv.Itoa(port))
+				dst := "http://" + addr + "/"
+				for _, o := range strings.Fields(opts) {
+					switch {
+					case o == "proto=tcp":
+						dst = "tcp://" + addr
+					case o == "proto=https":
+						dst = "https://" + addr
+					case strings.HasPrefix(o, "weight="):
+						weight = o[len("weight="):]
+					case strings.HasPrefix(o, "redirect="):
+						redir := strings.Split(o[len("redirect="):], ",")
+						if len(redir) == 2 {
+							dst = redir[1]
+							ropts = append(ropts, fmt.Sprintf("redirect=%s", redir[0]))
+						} else {
+							log.Printf("[ERROR] Invalid syntax for redirect: %s. should be redirect=<code>,<url>", o)
+							continue
+						}
+					default:
+						ropts = append(ropts, o)
+					}
+				}
+
+				cfg := "route add " + name + " " + route + " " + dst
+				if weight != "" {
+					cfg += " weight " + weight
+				}
+				if tags != "" {
+					cfg += " tags " + strconv.Quote(tags)
+				}
+				if len(ropts) > 0 {
+					cfg += " opts " + strconv.Quote(strings.Join(ropts, " "))
+				}
+				config = append(config, cfg)
+			}
+		}
+	}
+	return config
+}
